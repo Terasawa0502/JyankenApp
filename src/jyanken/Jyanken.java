76f4b4314e35f8092eb5d5afdaf2ae684c1e479d
寺澤徳隆 <mosqey1889@gmail.com>
2024-05-15 08:25:44 +0900

    ファーストコミット

---
+++ src/jyanken/Jyanken.java
@@ -0,0 +1,5 @@
+package jyanken;
+
+public class Jyanken {
+
+}
---
+++ src/jyanken/JyankenExec.java
@@ -0,0 +1,10 @@
+package jyanken;
+
+public class JyankenExec {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
